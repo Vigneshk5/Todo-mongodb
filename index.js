e48c9023efd73ebd9abc38545293cc0af4cc8202
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-03 21:34:27 +0530

    saved first value to mongodb

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority/test`
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority`
   );
   try {
     const kittySchema = new mongoose.Schema({
@@ -17,6 +17,7 @@ async function main() {
     const Kitten = mongoose.model("Kitten", kittySchema);
 
     const silence = new Kitten({ name: "Silence" });
+    await silence.save();
     console.log(silence.name);
   } finally {
     // await kitten.close();
