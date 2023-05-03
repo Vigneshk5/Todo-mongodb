ca6dc32c45c12586c80e40cc463f127c88de338e
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-03 21:22:26 +0530

    mongoose added

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 /** @format */
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri);
+const mongoose = require("mongoose");
+// const { MongoClient } = require("mongodb");
+// const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    const database = client.db("sample_mflix");
-    const movies = database.collection("movies");
+main().catch((err) => console.log(err));
 
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: "Back to the Future" };
-    const movie = await movies.findOne(query);
+async function main() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority/test`
+  );
 
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  const kittySchema = new mongoose.Schema({
+    name: String,
+  });
+
+  const Kitten = mongoose.model("Kitten", kittySchema);
+
+  const silence = new Kitten({ name: "Silence" });
+  console.log(silence.name);
 }
-run().catch(console.dir);
+
+// async function run() {
+//   try {
+//     const database = client.db("sample_mflix");
+//     const movies = database.collection("movies");
+
+//     // Query for a movie that has the title 'Back to the Future'
+//     const query = { title: "Back to the Future" };
+//     const movie = await movies.findOne(query);
+
+//     console.log(movie);
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
