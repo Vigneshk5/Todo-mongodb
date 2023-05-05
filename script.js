defe45a9eab8c948c32c02a18da4ac17e40457e3
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-05 19:28:07 +0530

    db model

--- script.js
+++ script.js
@@ -4,30 +4,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const todos = require("./models/todo");
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority`
+);
 
 app.use("/", express.static(path.resolve(__dirname, "assets")));
+
+app.use(bodyParser.json());
+
+app.post("/api/create", async (req, res) => {
+  const record = req.body;
+  console.log(record);
+  res.json({ status: "ok" });
+});
 
 app.listen(13371, () => {
   console.log("...server up");
 });
-
-main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.hnd64n8.mongodb.net/?retryWrites=true&w=majority`
-  );
-  try {
-    const kittySchema = new mongoose.Schema({
-      name: String,
-    });
-
-    const Kitten = mongoose.model("Kitten", kittySchema);
-
-    const silence = new Kitten({ name: "Silence" });
-    await silence.save();
-    console.log(silence.name);
-  } finally {
-    console.log("end");
-  }
-}
