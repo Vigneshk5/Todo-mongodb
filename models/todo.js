01faa661cf88963fbd65c0c08a1a8da9fd296484
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-05 19:27:25 +0530

    db model

---
+++ models/todo.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  record: { type: String, required: true },
+  date: { type: Number, default: 100 },
+});
+
+const model = mongoose.model("TodoModel", TodoSchema);
+model.exports = model;
